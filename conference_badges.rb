6016babaac71d00cbad9353c3fbcf08b83bdf4d6
Kizito Njoku <nkizito57@gmail.com>
2019-11-08 04:07:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,7 @@ end
 
 def assign_rooms(speakers)
   arr = []
+<<<<<<< HEAD
   rooms = 1
   speakers.each do |names| 
     arr << ("Hello, #{names}! You'll be assigned to room #{rooms}!")
@@ -25,4 +26,7 @@ def printer(array)
   batch_badge_creator(array).each {|id| puts id }
   assign_rooms(array).each {|id| puts id }
   
+=======
+  speakers.each do |names, room| {arr << "Hello, #{names}! You'll be assigned to room #{room}!"}
+>>>>>>> 3afbf8424d0e8478e09a1a50f5033f925eb5a756
 end
